374adfaf0c20e4f45b843e36ee89997c2bc9b13a
Duc Vu <duc.vh0407@gmail.com>
2025-05-22 09:56:29 +0700

    update

--- array_and_string/insert_array/_88_merge_sorted_array_solution2.java
+++ array_and_string/insert_array/_88_merge_sorted_array_solution2.java
@@ -2,7 +2,7 @@ package insert_array;
 
 public class _88_merge_sorted_array_solution2 {
     
-    public static void main(String[] args) {
-        System.out.println("done");
+    public void merge(int[] n1, m, int[] n2, n)
+    {
+        
     }
-}
